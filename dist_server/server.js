890477e624903d4f8a7e1181596f7cf4e00cdb84
Ricardo Reis <rjm.reis@gmail.com>
2017-01-05 13:19:41 +0100

    Typo fix

--- dist_server/server.js
+++ dist_server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const compression = require('compression');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // app.use(compression()); 
