c59e4725f81feea6bea8db5cd627a0b1f4c58f9f
Ricardo Reis <rjm.reis@gmail.com>
2017-01-05 12:52:19 +0100

    Added dist server

---
+++ dist_server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+// const compression = require('compression');
+
+const port = process.env.port || 3000;
+const app = express.createServer();
+
+// app.use(compression()); 
+app.use(express.static(__dirname));
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+/* eslint-disable no-console */
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server started port: ${port}`);
+  }
+});
